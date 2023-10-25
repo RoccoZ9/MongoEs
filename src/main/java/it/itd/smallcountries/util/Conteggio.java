36057ad39d93107d223abc70f5d654904dc1e05e
Roccoz <zagarocco9@outlook.it>
2023-10-25 11:37:14 +0200

    aggiunte costruttore, getters and setters della classe

--- src/main/java/it/itd/smallcountries/util/Conteggio.java
+++ src/main/java/it/itd/smallcountries/util/Conteggio.java
@@ -1,4 +1,27 @@
 package it.itd.smallcountries.util;
 
 public class Conteggio {
+    String _id;
+    Integer count;
+
+    public Conteggio(String _id, Integer count) {
+        this._id = _id;
+        this.count = count;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 }
